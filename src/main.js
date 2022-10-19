3b70c215e5df559c1b7d9dcd9b1026dd0aae7e87
DiegoPorta20 <93227425+DiegoPorta20@users.noreply.github.com>
2022-10-19 08:49:42 -0500

    feat: Insert the diferent option for example home and conversion rate

--- src/main.js
+++ src/main.js
@@ -1,19 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import PrimeVue from 'primevue/config';
-import router from "./router/index";
-import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
 
-import "primevue/resources/themes/md-light-indigo/theme.css";
-import "primevue/resources/primevue.min.css";
-import "primeicons/primeicons.css";
-
-const app = createApp(App);
-
-app.use(PrimeVue)
-app.use(router)
-// eslint-disable-next-line vue/multi-word-component-names
-app.component('Button', Button)
-app.component('InputText', InputText)
-app.use().mount('#app')
+createApp(App).mount('#app')
